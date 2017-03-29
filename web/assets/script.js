3d9620c0d6498b47359680731929b8a151ca8bf3
jeffersoncn <jefferson.nascimento3@fatec.sp.gov.br>
2017-03-29 13:44:37 -0300

    Adiciona assets do projeto

---
+++ web/assets/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function (e) {
+    $('.ui.form')
+        .form({
+            fields: {
+                capital: ['number', 'empty'],
+                taxa: ['number', 'empty'],
+                periodos: ['integer', 'empty'],
+            }
+        });
+
+    $('.card').hide().transition({
+        debug: true,
+        animation: 'fly left',
+        duration: 1000,
+        interval: 200
+    });
+
+     $('.swing-down').transition({
+        debug: true,
+        animation: 'swing down',
+        duration: 1000,
+        interval: 200
+    });
+
+    $('#resultados').transition({
+        debug: true,
+        animation: 'fade down',
+        duration: 600,
+        interval: 300
+    });
+
+    $('form').on('submit', function(e) {
+        $('.dimmer').toggleClass('active');
+    });
+});
