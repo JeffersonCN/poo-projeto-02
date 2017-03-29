ffea5d56df8d1112a3807e79907e7f9bf90cb600
jeffersoncn <jefferson.nascimento3@fatec.sp.gov.br>
2017-03-29 13:57:38 -0300

    Adicione página Home

--- src/java/br/com/juros/servlets/HomeServlet.java
+++ src/java/br/com/juros/servlets/HomeServlet.java
@@ -31,16 +31,61 @@ public class HomeServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HomeServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HomeServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            out.print("<!DOCTYPE html>");
+out.print("<html lang='pt-BR'>");
+out.print("<head>");
+out.print("    <title>Home - Projeto 02 - Programação Orientada a Objetos</title>");
+out.print("    <meta charset='UTF-8'>");
+out.print("    <meta name='viewport' content='width=device-width, initial-scale=1'>");
+out.print("    <link rel='stylesheet' type='text/css' href='assets/vendor/semantic/dist/semantic.min.css'>");
+out.print("    <link href='assets/style.css' rel='stylesheet'>");
+out.print("</head>");
+out.print("<body>");
+out.print("    <div class='ui visible inverted blue sidebar vertical labeled icon menu'>");
+out.print("        <a href='home' class='active item'>");
+out.print("            <i class='home left icon'></i> Home");
+out.print("        </a>");
+out.print("        <a href='juros-simples' class='item'>");
+out.print("            <i class='percent left icon'></i> Juros Simples");
+out.print("        </a>");
+out.print("        <a href='juros-composto' class='item'>");
+out.print("            <i class='signal left icon'></i> Juros Composto");
+out.print("        </a>");
+out.print("    </div>");
+out.print("    <div class='pusher'>");
+out.print("        <h1 class='ui header'>Home</h1>");
+out.print("        <div class='ui divider'></div>");
+out.print("        <div class='ui link cards'>");
+out.print("            <div class='blue card'>");
+out.print("                <a class='image' href='juros-simples'>");
+out.print("                    <img src='assets/images/juros-simples.jpg'>");
+out.print("                </a>");
+out.print("                <div class='content'>");
+out.print("                    <a class='header' href='juros-simples'>Juros Simples</a>");
+out.print("                    <div class='description'>");
+out.print("                        Quando o percentual de juros incide apenas sobre o valor principal.");
+out.print("                    </div>");
+out.print("                </div>");
+out.print("            </div>");
+out.print("            <div class='blue card'>");
+out.print("                <a class='image' href='juros-composto'>");
+out.print("                    <img src='assets/images//juros-composto.jpg'>");
+out.print("                </a>");
+out.print("                <div class='content'>");
+out.print("                    <a class='header' href='juros-composto'>Juros Composto</a>");
+out.print("                    <div class='description'>");
+out.print("                        Os juros gerados a cada período são incorporados ao principal para o cálculo dos juros do período seguinte.");
+out.print("                    </div>");
+out.print("                </div>");
+out.print("            </div>");
+out.print("        </div>");
+out.print("    </div>");
+out.print("    <script src='https://code.jquery.com/jquery-3.1.1.min.js' integrity='sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8='");
+out.print("        crossorigin='anonymous'></script>");
+out.print("    <script src='assets/vendor/semantic/dist/semantic.min.js'></script>");
+out.print("    <script src='assets/script.js'></script>");
+out.print("</body>");
+out.print("</html>");
         }
     }
 
